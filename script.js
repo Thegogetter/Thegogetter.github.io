e32dd9abd59946fcf4a492d497278d52227a3742
Thegogetter <111871301+Thegogetter@users.noreply.github.com>
2023-07-17 17:21:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const options = {
 	}
 };
 
+
+
+
 const getWeather=(city)=>{
 	cityName.innerHTML=city;
 fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`,options)
@@ -15,7 +18,7 @@ fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`,opt
 .then(response=> {
     
     console.log(response)
-    //cloud_pct.innerHTML=response.cloud_pct;
+    cloud_pct.innerHTML=response.cloud_pct;
 	temp.innerHTML=response.temp;
 	feels_like.innerHTML=response.feels_like;
 	windspeed2.innerHTML=response.wind_speed;
@@ -27,12 +30,70 @@ fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`,opt
 	sunrise.innerHTML=response.sunrise;
 	sunset.innerHTML=response.sunset;
 	humidity2.innerHTML=response.humidity;
-	temp2.innerHTML=response.temp;
+	
+
+	if(city=='Shangai'){
+		temps.innerHTML=response.temp;
+		feels.innerHTML=response.feels_like;
+		windspeeds.innerHTML=response.wind_speed;
+		hums.innerHTML=response.humidity;
+		mintemps.innerHTML=response.min_temp;
+		maxtemps.innerHTML=response.max_temp;
+		windspeeds.innerHTML=response.wind_speed;
+		winddegs.innerHTML=response.wind_degrees;
+		sunrises.innerHTML=response.sunrise;
+		sunsets.innerHTML=response.sunset;
+		cpcts.innerHTML=response.cloud_pct;
+	}
+
+	if(city=='Boston'){
+		tempb.innerHTML=response.temp;
+		feelb.innerHTML=response.feels_like;
+		windspeedb.innerHTML=response.wind_speed;
+		humb.innerHTML=response.humidity;
+		mintempb.innerHTML=response.min_temp;
+		maxtempb.innerHTML=response.max_temp;
+		windspeedb.innerHTML=response.wind_speed;
+		winddegb.innerHTML=response.wind_degrees;
+		sunriseb.innerHTML=response.sunrise;
+		sunsetb.innerHTML=response.sunset;
+		cpctb.innerHTML=response.cloud_pct;
+	}
+	if(city=='Mumbai'){
+		tempm.innerHTML=response.temp;
+		feelm.innerHTML=response.feels_like;
+		windspeedm.innerHTML=response.wind_speed;
+		humm.innerHTML=response.humidity;
+		mintempm.innerHTML=response.min_temp;
+		maxtempm.innerHTML=response.max_temp;
+		windspeedm.innerHTML=response.wind_speed;
+		winddegm.innerHTML=response.wind_degrees;
+		sunrisem.innerHTML=response.sunrise;
+		sunsetm.innerHTML=response.sunset;
+		cpctm.innerHTML=response.cloud_pct;
+	}
+	if(city=="Lucknow"){
+		templ.innerHTML=response.temp;
+		feell.innerHTML=response.feels_like;
+		windspeedl.innerHTML=response.wind_speed;
+		huml.innerHTML=response.humidity;
+		mintempl.innerHTML=response.min_temp;
+		maxtempl.innerHTML=response.max_temp;
+		windspeedl.innerHTML=response.wind_speed;
+		winddegl.innerHTML=response.wind_degrees;
+		sunrisel.innerHTML=response.sunrise;
+		sunsetl.innerHTML=response.sunset;
+		cpctl.innerHTML=response.cloud_pct;
+	}
 
 })
 .catch(err=>console.error(err));
 }
 
+getWeather('Shangai');
+getWeather('Boston');
+getWeather('Lucknow');
+getWeather('Mumbai');
 submit.addEventListener("click",(e)=>{
   e.preventDefault(); 
  getWeather(city.value); 
